d37f04158d63bca09e6201738e8d4ef5ba27ef05
Samuel Simões <samuelsimoes31@gmail.com>
2022-06-24 11:01:13 -0300

    feat: Header redirecting to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <div className={styles.Container}>
-      <Image src="/images/logo.svg" alt="logo" width={240} height={25} />
-    </div>
+    <Link href="/">
+      <div className={styles.Container}>
+        <Image src="/images/logo.svg" alt="logo" width={240} height={25} />
+      </div>
+    </Link>
   );
 }
